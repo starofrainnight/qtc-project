3aec25c2cb6c3d99cbce6ee9c90fc0d0a0ca6560
Hong-She Liang <sorn@taorules.com>
2023-11-19 22:32:19 +0800

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -6,55 +6,51 @@
 import io
 from setuptools import setup, find_packages
 
-with io.open('README.rst', encoding='utf-8') as readme_file, io.open(
-    'HISTORY.rst', encoding='utf-8'
+with io.open("README.rst", encoding="utf-8") as readme_file, io.open(
+    "HISTORY.rst", encoding="utf-8"
 ) as history_file:
-    long_description = (readme_file.read() + "\n\n" + history_file.read())
+    long_description = readme_file.read() + "\n\n" + history_file.read()
 
 install_requires = [
-    'click>=6.0',
-    # TODO: put package requirements here
+    "click>=6.0",
+    "pathspec",
 ]
 
 setup_requires = [
-    'pytest-runner',
+    "pytest-runner",
     # TODO(sorn): put setup requirements (distutils extensions, etc.) here
 ]
 
 tests_requires = [
-    'pytest',
+    "pytest",
     # TODO: put package test requirements here
 ]
 
 setup(
-    name='qtc-project',
+    name="qtc-project",
     version="0.0.1",
     description="A program use for maintain the general qt projects",
     long_description=long_description,
     author="Hong-She Liang",
-    author_email='sorn@taorules.com',
-    url='https://github.com/sorn/qtc-project',
+    author_email="sorn@taorules.com",
+    url="https://github.com/starofrainnight/qtc-project",
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'qtc-project=qtcproject.__main__:main'
-        ]
-    },
+    entry_points={"console_scripts": ["qtc-project=qtcproject.__main__:main"]},
     include_package_data=True,
     install_requires=install_requires,
     license="Apache Software License",
     zip_safe=False,
-    keywords='qtcproject,qtc-project',
+    keywords="qtcproject,qtc-project",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    test_suite='tests',
+    test_suite="tests",
     tests_require=tests_requires,
     setup_requires=setup_requires,
 )
